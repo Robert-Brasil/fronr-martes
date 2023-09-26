13dca4d92a32d4f3f1b605ee4a30de096a59fdd7
plopezproyectos <96803656+plopezproyectos@users.noreply.github.com>
2023-09-26 19:57:05 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+
+  <div class="hola">
+      hola
+  </div>
+
+  <button>apretar</button>
+    <MiComponente />
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MiComponente from './components/MiComponente.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MiComponente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  background-color: blue;
+}
+
+.hola{
+  border: 5px red solid;
+}
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>
+        Mi componente
+</h1>
+
+</template>
+
+<style scoped>
+    h1{
+        color: red
+    }
+</style>
