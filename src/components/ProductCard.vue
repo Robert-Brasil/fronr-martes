8a3d0ae50cf22b2d43c0e044fd36cc5b90f58eb4
plopezproyectos <96803656+plopezproyectos@users.noreply.github.com>
2023-10-31 19:39:11 -0300

    agrego ProductCard

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -6,6 +6,7 @@ condicion && true
 
 <template>
   <div class="contenedor">
+
     <div
       :class="{
         'contenedor__variable--chico': !ver,
@@ -14,50 +15,102 @@ condicion && true
       @mouseover="ver = true"
       @mouseleave="ver = false"
     >
-      <h3 v-if="ver">in stock</h3>
-      <img :src="producto.image" alt="imagen" class="imagen" />
-      <div class="estrellas">
-        <div class="puntos" puntaje="5"></div>
-        <img src="star.svg" class="estrella" />
-        <img src="star.svg" class="estrella" />
-        <img src="star.svg" class="estrella" />
-        <img src="star.svg" class="estrella" />
-        <img src="star.svg" class="estrella" />
-      </div>
-      <h3 class="reviews">
-        {{ producto.rating.rate }}
+
+      <h3
+        :class="{
+          contenedor__variable__stock: true,
+          borrar: !verBotones,
+          mostrar: verBotones,
+        }"
+      >
+      <img src="../assets/ProductCard/ok.svg" alt="">
+        in stock
       </h3>
+
+      <img :src="producto.image" alt="imagen" class="imagen" />
       <p>{{ producto.title }}</p>
       <h3>${{ producto.price }}</h3>
-      <button v-if="ver">Add To Cart</button>
+      <button v-if="verBotones" class="contenedor__variable__add">Add To Cart</button>
+        <RatingVue :rate="producto.rating.rate * 20 + '%'" ></RatingVue>
+
     </div>
   </div>
 </template>
 
 <script>
+import RatingVue from './RatingVue.vue';
+
 export default {
   name: "ProductoCard",
   props: {
     prueba: String,
-    producto: Array
+    producto: Array,
   },
+    components: {
+        RatingVue
+    },
   data() {
     return {
       resultado: 0,
       numeroDeCalculo: 1,
       verCalculadora: true,
       ver: false,
-     
+      verBotones: false,
     };
+  },
+  methods: {
+    mostrarBotones() {
+      if (this.ver) {
+        setTimeout(() => {
+          this.verBotones = true;
+        }, 500);
+      } else {
+        this.verBotones = false;
+      }
+    },
+  },
+  watch: {
+    ver() {
+      this.mostrarBotones();
+    },
   },
 };
 </script>
 
 <style scoped>
+.borrar {
+  visibility: hidden;
+  opacity: 0%;
+  transition: 0.2s;
+}
+
+.mostrar {
+  visibility: visible;
+  opacity: 100%;
+  transition: 0.5s;
+}
+
+.contenedor__variable__stock {
+  margin: auto;
+  width: 200px;
+  height: 20px;
+  transition: 0.5s;
+  position: absolute;
+  left: -50px;
+  top: 10px;
+  
+}
+
 .contenedor {
   width: 235px;
   height: 319px;
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
 }
 
 .contenedor__variable--chico {
@@ -65,19 +118,38 @@ export default {
   width: 235px;
   height: 319px;
   position: absolute;
+  top: 0px;
   border: 1px solid #dddddd;
-    background-color: #ffffff;
+    padding-top: 0px;
+
 }
 
 .contenedor__variable--grande {
   transition: 0.5s;
   position: absolute;
-  top: -20px;
+  top: -40px;
   width: 235px;
   height: 400px;
   border: 1px solid #dddddd;
   z-index: 1;
   background-color: #ffffff;
+  padding-top: 26px;
+}
+
+.contenedor__variable__add{
+  position: absolute;
+  border-radius: 50px;
+border: 2px solid var(--Color---3, #0156FF);
+color: var(--Color---3, #0156FF);
+font-family: Poppins;
+font-size: 14px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+padding: 8px 20px;
+bottom: 17px;
+left: 32px;
+width: 161px;
 }
 
 @keyframes example {
@@ -91,23 +163,5 @@ export default {
 .imagen {
   width: 150px;
   height: 150px;
-}
-.estrellas {
-  display: flex;
-  flex-direction: row;
-  position: relative;
-  width: min-content;
-}
-.estrella {
-  width: 13px;
-  height: 13px;
-  border: solid black 1px;
-}
-.puntos {
-  position: absolute;
-  background-color: yellow;
-  height: 20px;
-  width: 100%;
-  z-index: -1;
 }
 </style>
